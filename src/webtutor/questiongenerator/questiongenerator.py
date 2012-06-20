51225f687d375657788d188b0dc7c9e844163f36
gudmundur-heimisson <gudmundur.heimisson@gmail.com>
2012-06-20 10:31:30 +0000

    No longer an abstract class. Implemented getNextQuestion. Wrote test code.

--- src/webtutor/questiongenerator/questiongenerator.py
+++ src/webtutor/questiongenerator/questiongenerator.py
@@ -1,12 +1,11 @@
-from abc import ABCMeta, abstractmethod
-from webtutor.random import seed
+from webtutor.random import seed, sample
+from webtutor import Question
 
 class QuestionGenerator(object):
     '''
-    Abstract base class for *QuestionGenerator classes. These classes generate randomized questions given some input parameters.
+    Base class for *QuestionGenerator classes. These classes generate randomized questions given some input parameters.
     '''
 
-    __metaclass__ = ABCMeta
 
     def __init__(self, titles=None, bodies=None, answers=None,
                  explanations=None, questionID=0,
@@ -26,6 +25,10 @@ class QuestionGenerator(object):
         self.bodies = bodies
         self.answers = answers
         self.explanations = explanations
+        # These are things that can vary when a new question is selected.
+        # For example, a new question will always have a predictable ID,
+        # or language, but these will be chosen fresh each iteration.
+        self.choices = [self.titles, self.bodies, self.answers, self.explanations]
         self.questionID = questionID
         self.language = language
         self.randomSeed = randomSeed
@@ -41,11 +44,50 @@ Question ID: {questionID!r}
 Language: {language!r}
 Random Seed: {randomSeed!r}'''.format(**self.__dict__)
 
-    @abstractmethod
+    def _getQChoices(self):
+        # Make sure everything is either length 1, or the same length.
+        lengths = [len(x) for x in self.choices]
+        maxLength = 1
+        for l in lengths:
+            if l == 1 or l == maxLength:
+                continue
+            elif l != maxLength:
+                if maxLength == 1:
+                    maxLength = l
+                else: # This means maxLength is not 1, and l is not 1 or the same as maxLength
+                    raise ValueError('Lists are inconsistent lengths.')
+        for i in range(len(self.choices)):
+            c = self.choices[i]
+            if len(c) == 1:
+                self.choices[i] = [c[0] for x in range(maxLength)]
+        self.qChoices = zip(*self.choices)
+        return self.qChoices
+                
+
     def getNextQuestion(self):
         '''
         Generates a new question, and returns it as a Question object. Then increments questionID.
         '''
-        pass
+        q = Question()
+        self._getQChoices()
+        c = sample(self.qChoices)
+        q.title = c[0]
+        q.body = c[1]
+        q.answer = c[2]
+        q.explanation = c[3]
+        q.id = self.questionID
+        self.questionID += 1
+        return q
+    
+    
+if __name__ == '__main__':
+    n = 10
+    titles = ['Title %s' % i for i in range(1)]
+    bodies = ['Body %s' % i for i in range(n)]
+    answers = ['Answer %s' % i for i in range(n)]
+    explanations = ['Explanation %s' % i for i in range(1)]
+    qgen = QuestionGenerator(titles, bodies, answers, explanations, 42, 'en', 24)
+    for i in range(20):
+        print qgen.getNextQuestion(), '\n'
     
     
