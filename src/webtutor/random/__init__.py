1d8a8c8cf59de2e8a6927f437baa124e473c79c7
gudmundur-heimisson <gudmundur.heimisson@gmail.com>
2012-06-20 13:21:51 +0000

    Working on the fractions function.

--- src/webtutor/random/__init__.py
+++ src/webtutor/random/__init__.py
@@ -1,5 +1,6 @@
 import rpy2.robjects as robj
 import numpy.random as rand
+from collections import Counter
 
 def seed(x):
     rand.seed(x)
@@ -8,6 +9,7 @@ def seed(x):
 
 def sample(x, size=1, replace=False, p=None):
     ''' Mask for corresponding R function.'''
+    x = list(x)
     indices = robj.IntVector(range(len(x)))
     if p is not None:
         p_R = robj.FloatVector(p)
@@ -19,19 +21,46 @@ def sample(x, size=1, replace=False, p=None):
     else:
         return [x[i] for i in sampleIndices]
 
-def _getFraction(numerators, denominators):
-    # Make sure it's guaranteed to get a fraction that
-    # is not an integer.
-    num = sample(numerators)
-    den = sample(denominators)
+def _getFraction(num, den):
+    pass
     
 
-def fractions(size=1, replace=False, numerators=[1,2,3,5,7], denominators=[2,3,5,7]):
+def fractions(size=1, replace=False, numerators=[1,2,3,5,7], denominators=[1,2,3,5,7]):
     ''' Returns randomly generated sympy Fractions.
     Guaranteed to be not an integer.'''
+    # Make sure it's guaranteed to get a fraction, i.e.,
+    # numerators and denominators are not all the same number,
+    # and denominators is not just 1.
+    cNums = Counter(numerators)
+    cDens = Counter(denominators)
+    nks = cNums.keys()
+    dks = cDens.keys()
+    if (len(nks) == 1 and len(dks) == 1 and dks == nks) or (len(dks) == 1 and dks[0] == 1):
+        raise ValueError('Impossible to create a fraction with given numerators and denominators.')
+    # Make sure 1 isn't in there
+    cDens.pop(1)
+    if not replace: # Make sure it is possible to generate enough fractions
+        pass
+    print cDens
+    denominators = list(cDens)
+    nums = sample(numerators, size, replace)
+    dens = sample(denominators, size, replace)
+    # Have to generate fractions so that there aren't any integers
+    cNums = Counter(nums)
+    cDens = Counter(dens)
+    print cNums, cDens
+    # Make sure that it's possible to not have any integers
+    numKeys = cNums.keys()
+    denKeys = cDens.keys()
+    for nk in numKeys:
+        # If there are not enough possible denominators that are not
+        # nk, then it is not possible to not make an integer
+        pass
     
 
 if __name__ == '__main__':
     a = ['Sample %s' % i for i in range(1,11)]
     p = [i for i in range(1,11)]
     print sample(a, 4, True, p)
+    for f in fractions(5, True):
+        print f
